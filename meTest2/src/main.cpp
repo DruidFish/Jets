694cac2121ffd68c71e77f96d2fb5b5f46f50de3
Ben Wynne <bwynne@cern.ch>
2016-09-29 15:09:05 +0200

    Made an exact version using TLVs throughout, and moved the arrays version to test 2 as a start of migrating to a vector/GPU version

--- meTest2/src/main.cpp
+++ meTest2/src/main.cpp
@@ -17,14 +17,21 @@ bool SortJetsByPt( TLorentzVector const& i, TLorentzVector const& j )
 //Try to make some jets myself
 int main( int argc, char * argv[] )
 {
-	vector< TLorentzVector > inputs, outputs;
+	vector< TLorentzVector > outputs;
+
+	//Dummy TLV for conversions
+	TLorentzVector dummy1;
+	TLorentzVector dummy2;
 
 	//Read the fastjet example input
 	double px, py, pz, E;
+	vector< double > phis, rapidities, pts;
 	while ( cin >> px >> py >> pz >> E )
 	{
-		inputs.push_back( TLorentzVector() );
-		inputs.back().SetPxPyPzE( px, py, pz, E );
+		dummy1.SetPxPyPzE( px, py, pz, E );
+		phis.push_back( dummy1.Phi() );
+		rapidities.push_back( dummy1.Rapidity() );
+		pts.push_back( dummy1.Pt() );
 	}
 
 	//kt^2n = pt^2n //for single objects
@@ -35,22 +42,19 @@ int main( int argc, char * argv[] )
 	double const aD2 = 1.0 / ( D * D );
 
 	//Loop over all objects
-	while ( inputs.size() )
+	while ( phis.size() )
 	{
 		double kt2Min = DBL_MAX;
 		unsigned int thisMinIndex = 0;
 		unsigned int pairMinIndex = 0;
-		unsigned int const totalObjects = inputs.size();
-
-		//Loop over all input objects
+		unsigned int const totalObjects = phis.size();
 		for ( unsigned int thisObjectIndex = 0; thisObjectIndex < totalObjects; thisObjectIndex++ )
 		{
 			//Single-object values
-			TLorentzVector * thisObject = &inputs[ thisObjectIndex ];
-			double const thisApt = 1.0 / thisObject->Pt();
+			double const thisApt = 1.0 / pts[ thisObjectIndex ];
 			double const thisApt2 = thisApt * thisApt;
-			double const thisPhi = thisObject->Phi();
-			double const thisRapidity = thisObject->Rapidity();
+			double const thisPhi = phis[ thisObjectIndex ];
+			double const thisRapidity = rapidities[ thisObjectIndex ];
 
 			//Update minimum kt^2 value
 			if ( thisApt2 < kt2Min )
@@ -64,11 +68,10 @@ int main( int argc, char * argv[] )
 			for ( unsigned int pairObjectIndex = thisObjectIndex + 1; pairObjectIndex < totalObjects; pairObjectIndex++ )
 			{
 				//Pair values
-				TLorentzVector * pairObject = &inputs[ pairObjectIndex ];
-				double const pairApt = 1.0 / pairObject->Pt();
+				double const pairApt = 1.0 / pts[ pairObjectIndex ];
 				double const pairApt2 = pairApt * pairApt;
-				double const deltaPhi = thisPhi - pairObject->Phi();
-				double const deltaRapidity = thisRapidity - pairObject->Rapidity();
+				double const deltaPhi = thisPhi - phis[ pairObjectIndex ];
+				double const deltaRapidity = thisRapidity - rapidities[ pairObjectIndex ];
 				double kt2 = ( ( deltaPhi * deltaPhi ) + ( deltaRapidity * deltaRapidity ) ) * aD2;
 				if ( pairApt2 < thisApt2 )
 				{
@@ -92,13 +95,27 @@ int main( int argc, char * argv[] )
 		//Single objects as min kt are outputs, pairs get merged
 		if ( thisMinIndex == pairMinIndex )
 		{
-			outputs.push_back( inputs[ thisMinIndex ] );
-			inputs.erase( inputs.begin() + thisMinIndex );
+			//note rapidity != eta...
+			outputs.push_back( TLorentzVector() );
+			outputs.back().SetPtEtaPhiM( pts[ thisMinIndex ], rapidities[ thisMinIndex ], phis[ thisMinIndex ], 0.0 );
+
+			phis.erase( phis.begin() + thisMinIndex );
+			rapidities.erase( rapidities.begin() + thisMinIndex );
+			pts.erase( pts.begin() + thisMinIndex );
+
 		}
 		else
 		{
-			inputs[ thisMinIndex ] += inputs[ pairMinIndex ];
-			inputs.erase( inputs.begin() + pairMinIndex );
+			//note rapidity != eta...
+			dummy1.SetPtEtaPhiM( pts[ thisMinIndex ], rapidities[ thisMinIndex ], phis[ thisMinIndex ], 0.0 );
+			dummy2.SetPtEtaPhiM( pts[ pairMinIndex ], rapidities[ pairMinIndex ], phis[ pairMinIndex ], 0.0 );
+			dummy1 += dummy2;
+			pts[ thisMinIndex ] = dummy1.Pt();
+			rapidities[ thisMinIndex ] = dummy1.Rapidity();
+			phis[ thisMinIndex ] = dummy1.Phi();
+			phis.erase( phis.begin() + pairMinIndex );
+			rapidities.erase( rapidities.begin() + pairMinIndex );
+			pts.erase( pts.begin() + pairMinIndex );
 		}
 	}
 
